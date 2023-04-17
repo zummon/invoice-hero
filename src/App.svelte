34fdc770205ebea9773d54dfaf6b3e7007e94ba1
Teerapat Anantarattanachai <zummon.space@gmail.com>
2023-04-17 02:49:24 +0000

    Update .gitignore and App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -4,7 +4,6 @@
 
 	let l = data[""].label[""];
 	let q = data[""].q;
-	let qs = "";
 
 	const price = number => {
 	  number = Number(number);
@@ -59,22 +58,6 @@
 	  q = { ...data[q.lang].q, ...obj };
 	});
 
-	$: {
-	  let str = "?";
-	  Object.keys(q).forEach(key => {
-	    const values = q[key];
-	    if (values) {
-	      if (Array.isArray(values)) {
-	        values.forEach(value => {
-	          str += `${key}=${value}&`;
-	        });
-	        return;
-	      }
-	      str += `${key}=${values}&`;
-	    }
-	  });
-	  qs = str;
-	}
 	$: l = {
 	  ...data[q.lang].label[""],
 	  ...data[q.lang].label[q.doc]
